69d08705f433621a10596ed39437a65cadc6ac27
Ahmed Hamed <Dom1997ain@gmail.com>
2026-06-03 19:32:44 +0300

    Remove duplicate gradlePlugin block from build-logic root
    Only build-logic/convention should register plugins; root was causing
    :jar task to generate plugin descriptors without implementation classes

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -6,35 +6,5 @@ dependencies {
     implementation(libs.agp)
     implementation(libs.kotlin.gradle)
     implementation(libs.ksp)
-    implementation(libs.hilt.gradle)
     implementation(libs.room)
-}
-
-gradlePlugin {
-    plugins {
-        register("floosiAndroidApplication") {
-            id = "floosi.android.application"
-            implementationClass = "com.floosi.buildlogic.FloosiAndroidApplicationPlugin"
-        }
-        register("floosiAndroidLibrary") {
-            id = "floosi.android.library"
-            implementationClass = "com.floosi.buildlogic.FloosiAndroidLibraryPlugin"
-        }
-        register("floosiAndroidLibraryCompose") {
-            id = "floosi.android.library.compose"
-            implementationClass = "com.floosi.buildlogic.FloosiAndroidLibraryComposePlugin"
-        }
-        register("floosiAndroidHilt") {
-            id = "floosi.android.hilt"
-            implementationClass = "com.floosi.buildlogic.FloosiAndroidHiltPlugin"
-        }
-        register("floosiAndroidRoom") {
-            id = "floosi.android.room"
-            implementationClass = "com.floosi.buildlogic.FloosiAndroidRoomPlugin"
-        }
-        register("floosiJvmLibrary") {
-            id = "floosi.jvm.library"
-            implementationClass = "com.floosi.buildlogic.FloosiJvmLibraryPlugin"
-        }
-    }
 }
